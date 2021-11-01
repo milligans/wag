09a0e3412c52a6b3f97aae003ec89ccd7c18c716
korova_blue@hotmail.com <73536360+milligans@users.noreply.github.com>
2021-11-01 08:14:07 +0000

    Initial commit

---
+++ first_wagtail/first_wagtail/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-2r)qr1mope^$u7vc7voli$yxy)03vtvz13#lsbi=z62s)h!!6('
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
